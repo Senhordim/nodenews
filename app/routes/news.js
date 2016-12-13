7974f338a963c01f8fca755c184cf8707ed3e364
Diego Collares <diego.collares@gmail.com>
2016-12-13 13:10:14 -0200

    isolando conexão com o mysql

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,21 +1,14 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
+
+  var connection = dbConnection();
 
   app.get('/notices', function(req, res){
 
-    var mysql = require('mysql');
-
-    var connection = mysql.createConnection({
-      host : 'localhost',
-      user : 'root',
-      password: '',
-      database : 'node_news'
-    });
-
     connection.query('select * from news', function(error, result){
-      res.send(result)
+      res.render("news/notices", {notices : result})
     });
-
-    // res.render("news/notices");
 
   });
 
