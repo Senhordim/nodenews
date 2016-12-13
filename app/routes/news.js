38460ee2eedf78c1151587f32abdffe196e6284c
Diego Collares <diego.collares@gmail.com>
2016-12-13 12:26:53 -0200

    config and connection with mysql

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,5 +1,22 @@
 module.exports = function(app){
+
   app.get('/notices', function(req, res){
-    res.render("news/notices");
+
+    var mysql = require('mysql');
+
+    var connection = mysql.createConnection({
+      host : 'localhost',
+      user : 'root',
+      password: '',
+      database : 'node_news'
+    });
+
+    connection.query('select * from news', function(error, result){
+      res.send(result)
+    });
+
+    // res.render("news/notices");
+
   });
-}
+
+};
