2eb540d6bdaa40d52b6cebd4e66c4010f66b172b
Diego Collares <diego.collares@gmail.com>
2016-12-13 13:39:03 -0200

    config routes to project with consign module

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = require('./config/server');
 
-var routeNews = require('./app/routes/news')(app);
-var routeHome = require('./app/routes/home')(app);
-var routeAdmin = require('./app/routes/admin')(app);
-
 app.listen(3000, function(){
   console.log("Servidor on");
 });
--- config/server.js
+++ config/server.js
@@ -1,13 +1,16 @@
 // Require Express para dentro do Projeto
 var express = require('express');
+var consign = require('consign');
 
 // Executando a função express
 var app = express();
 
 // Setando ejs como engine de view
 app.set('view engine', 'ejs');
-
 app.set('views', './app/views')
+
+// config routes to project with consign module
+consign().include('app/routes').into(app);
 
 // Exportando modulo
 module.exports = app;
